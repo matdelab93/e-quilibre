f38bf6f482616fe2ed1a3e92fc7620d73541ab32
Christophe Equilibre <christophet.equilibre@gmail.com>
2025-03-26 13:28:00 +0000

    delete tables inutiles

--- models/mart/analyse_recharge_de_véhicule.sql
+++ models/mart/analyse_recharge_de_véhicule.sql
@@ -2,9 +2,9 @@ SELECT
     puissance_nominale,
     COUNT(*) AS nombre_de_bornes,
     CASE 
-        WHEN puissance_nominale >= 10 AND puissance_nominale < 50 THEN 'Borne standard'
-        WHEN puissance_nominale >= 50 AND puissance_nominale < 150 THEN 'Borne rapide'
-        WHEN puissance_nominale >= 150 THEN 'Borne ultra-rapide'
+        WHEN puissance_nominale >= 10 AND puissance_nominale < 50 THEN 'Borne standard (10 à 50 kW)'
+        WHEN puissance_nominale >= 50 AND puissance_nominale < 150 THEN 'Borne rapide (50 à 150kW)'
+        WHEN puissance_nominale >= 150 THEN 'Borne ultra-rapide (> à 150kW)'
     END AS categorie_puissance,
     CONCAT(
         FLOOR(60 / (puissance_nominale * 0.9)), 'h ', 
